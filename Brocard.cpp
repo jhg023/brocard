76afd076d6f90b5bb839ff187d0b582f71627445
Jacob Glickman <jhg023@bucknell.edu>
2019-11-24 19:41:02 -0500

    Readability / optimizations

--- Brocard.cpp
+++ Brocard.cpp
@@ -122,39 +122,33 @@ static inline uint64_t initialize_factorial( uint64_t n, uint64_t prime, uint64_
   return factorial % prime;
 }
 
-#define CNST_LIMB( C ) ( ( uint64_t ) C##LL )
-#define MP_LIMB_T_MAX ( ~( uint64_t ) 0 )
-#define GMP_LIMB_HIGHBIT ( MP_LIMB_T_MAX ^ ( MP_LIMB_T_MAX >> 1 ) )
-#define LIMB_HIGHBIT_TO_MASK( n )                                                                                                                    \
-  ( ( ( int64_t ) -1 >> 1 ) < 0 ? ( int64_t )( n ) >> ( 63 ) : ( n ) &GMP_LIMB_HIGHBIT ? MP_LIMB_T_MAX : CNST_LIMB( 0 ) )
-
 static inline int jacobi_unsigned( uint64_t a, uint64_t b ) {
-  int c, bit = 0;
-
-  bit >>= 1;
   b >>= 1;
-  c = __builtin_ctzll( a );
+
+  int bit = 0;
+  int c = __builtin_ctzll( a );
+
   bit ^= c & ( b ^ ( b >> 1 ) );
+  
   a >>= c;
   a >>= 1;
 
-  do {
-    uint64_t t = a - b;
-    uint64_t bgta = LIMB_HIGHBIT_TO_MASK( t );
+  int64_t t;
+  uint64_t bgta;
 
-    if( t == 0 ) {
-      return 0;
-    }
+  do {
+    t = a - b;
+    bgta = t >> 63;
     bit ^= ( bgta & a & b );
     b += ( bgta & t );
     a = ( t ^ bgta ) - bgta;
     c = __builtin_ctzll( t );
-    c++;
+    ++c;
     bit ^= c & ( b ^ ( b >> 1 ) );
     a >>= c;
-  } while( b > 0 );
+  } while( b != 0 );
 
-  return 1 - 2 * ( bit & 1 );
+  return 1 - ( ( bit & 1 ) << 1 );
 }
 
 static inline void *brocard( void *arguments ) {
